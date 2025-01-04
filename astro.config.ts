52be4bdf22347dedc46f85f830635df8ea1aca0b
Shane-Donlon <shane.donlon@live.com>
2025-01-04 13:52:39 +0000

    vercel adapter add

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,9 @@
+import vercel from '@astrojs/vercel/serverless';
 import qwikdev from "@qwikdev/astro";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [qwikdev()],
+  adapter: vercel(),
 });
